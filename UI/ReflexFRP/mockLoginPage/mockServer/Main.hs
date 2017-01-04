aebf89d6730d174512b09ed9dfc43b58f6cb10c8
meditans <meditans@gmail.com>
2017-01-04 17:48:45 +0100

    Update the server to return either an error or the user

--- UI/ReflexFRP/mockLoginPage/mockServer/Main.hs
+++ UI/ReflexFRP/mockLoginPage/mockServer/Main.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE OverloadedStrings, TypeApplications #-}
 
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# OPTIONS_GHC -fdefer-typed-holes #-}
+
 module Main where
 
 import MockAPI
@@ -10,9 +13,12 @@ import Control.Concurrent (threadDelay)
 import Control.Monad.IO.Class
 import qualified Data.Map as M
 import Network.Wai.Middleware.Gzip
+import Shaped
+import Generics.SOP
+import Data.Functor.Const
 
 server :: Server MockApi
-server = authenticate :<|> serveAssets :<|> serveJS
+server = authenticate'' :<|> serveAssets :<|> serveJS
   where
     serveAssets = serveDirectory "../mockClient/assets"
     serveJS = serveDirectory "../mockClient/js/"
@@ -30,15 +36,38 @@ authenticate u
     correctInfo = M.lookup (userMail u) users == Just (userPassword u)
     userPresent = userMail u `elem` M.keys users
 
+authenticate' :: (Monad m, MonadIO m) => User -> m (Either (UserShaped (Const (Maybe Text))) User)
+authenticate' u
+  | correctInfo = do
+      liftIO (threadDelay 1000000)
+      return (Right u)
+  | not userPresent = do
+      liftIO (threadDelay 1000000)
+      return . Left $ UserShaped (Const $ Just "The user mail is not present") (Const Nothing)
+  | otherwise = do
+      liftIO (threadDelay 1000000)
+      return . Left $ UserShaped (Const Nothing) (Const $ Just "The password is wrong")
+  where
+    users = M.fromList [ ("user1@gmail.com", "pass1")
+                       , ("user2@gmail.com", "pass2")
+                       , ("user3@gmail.com", "pass3")
+                       ]
+    correctInfo = M.lookup (userMail u) users == Just (userPassword u)
+    userPresent = userMail u `elem` M.keys users
+
+authenticate'' :: (Monad m, MonadIO m) => User -> m (Either (UserShaped (Const (Maybe Text))) User)
+authenticate'' u = case transfGen $ validateRecord u clientValidation of
+  Left  a -> return $ Left a
+  Right b -> authenticate' b
+
 main :: IO ()
 main = run 8081 (gzip gzipSettings $ serve (Proxy @MockApi) server)
--- main = run 8081 (serve (Proxy @MockApi) server)
   where
     gzipSettings = def { gzipFiles = GzipCompress }
 
 ---------------------- Validations
 
--- serverOnlyValidation :: UserShaped Validation
+-- serverOnlyValidation :: UserShaped (Validation (Either Text))
 -- serverOnlyValidation = UserShaped
---   (Validation . Compose $ \m -> if m == "meditans@gmail.com" then Nothing else Just "User unknown")
---   (Validation . Compose $ \p -> if p == "password"           then Nothing else Just "Wrong password")
+--   (Validation . Comp $ \m -> if m `notElem` M.keys userDatabase then Right m else Left "Mail not present in the database")
+--   (Validation . Comp $ \p -> if p /= "" then Right p else Left "Blank password")
