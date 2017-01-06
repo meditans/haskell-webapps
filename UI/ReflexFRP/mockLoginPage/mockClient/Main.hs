a5b403310c35a2d737ee989afadca2fa16374db9
meditans <meditans@gmail.com>
2017-01-06 17:10:13 +0100

    Add another parameter to formlet to force the valutation of errors

--- UI/ReflexFRP/mockLoginPage/mockClient/Main.hs
+++ UI/ReflexFRP/mockLoginPage/mockClient/Main.hs
@@ -41,8 +41,8 @@ apiClients = client (Proxy @MockApi) (Proxy @m) (constDyn url)
   where url :: BaseUrl
         url = BaseFullUrl Http "localhost" 8081 ""
 
-invokeAPI :: forall t m . (MonadWidget t m) => Endpoint t m
-invokeAPI :<|> _ = apiClients
+authEndpoint :: (MonadWidget t m) => Endpoint t m
+authEndpoint :<|> _ = apiClients
 
 --------------------------------------------------------------------------------
 -- Body of the form
@@ -51,9 +51,9 @@ body :: forall t m. MonadWidget t m => m ()
 body = void . divClass "login-clean" . el "form" $ do
   hiddenTitle
   icon
-  (_, serverResponse) <- form userWidget clientValidation invokeAPI
+  (_, serverResponse) <- form userWidget clientValidation authEndpoint
   forgotYourUsername
-  el "h3" (dynText =<< feedback serverResponse)
+  el "div" $ el "label" (dynText =<< feedback serverResponse)
 
 --------------------------------------------------------------------------------
 -- Implementation of the visual elements:
@@ -107,15 +107,20 @@ feedback e = holdDyn "" . ffor e $
 
 -- Forms for the shaped approach: This should be supplied by the user, as it's a
 -- rendering of the particular markup the user wants for the form.
-userWidgetInternal :: MonadWidget t m => TextInputConfig t -> Dynamic t (Maybe Text) -> m (Dynamic t Text)
-userWidgetInternal conf err = do
+userWidgetInternal :: MonadWidget t m
+                   => TextInputConfig t      -- ^ Special tags for input fields
+                   -> Event t ()             -- ^ Event that forces the display of errors
+                   -> Dynamic t (Maybe Text) -- ^ Dynamic representing the error
+                   -> m (Dynamic t Text)
+userWidgetInternal conf upd err = do
   textBox   <- textInput conf
   firstBlur <- headE $ select (textBox ^. textInput_builderElement
                                         . to _inputElement_element
                                         . to _element_events)
                               (WrapArg Blur)
-  displayedErr <- join <$> holdDyn (constDyn Nothing) (err <$ firstBlur)
-  el "error" $ dynText (maybe "" id <$> displayedErr)
+  let b = leftmost [() <$ firstBlur, upd]
+  displayedErr <- join <$> holdDyn (constDyn Nothing) (err <$ b)
+  el "label" $ dynText (maybe "" id <$> displayedErr)
   return (value textBox)
 
 --------------------------------------------------------------------------------
@@ -140,10 +145,11 @@ form :: MonadWidget t m
             -> m ( Dynamic t (Either (UserShaped (Const (Maybe Text))) User)
                  , Event t (Either Text (Either (UserShaped (Const (Maybe Text))) User)) )
 form shapedWidget clientVal endpoint = mdo
+  postBuild <- getPostBuild
   -- Here I read a tentative user from the created interface
-  rawUser <- createInterface (splitShaped errorEvent) shapedWidget
+  rawUser <- createInterface (splitShaped errorEvent) send shapedWidget
   -- Here I define the button. This could probably be mixed with the button code
-  send <- buttonElement send responseEvent
+  send <- buttonElement send (() <$ serverResponse)
   -- This part does the server request and parses back the response without
   -- depending on the types in servant-reflex
   serverResponse <- let query = either (const $ Left "Please fill correctly the fields above") Right <$> validationResult
@@ -151,15 +157,16 @@ form shapedWidget clientVal endpoint = mdo
   let
     -- Validation result is the rawUser ran through the validation
     validationResult = transfGen . flip validateRecord clientVal <$> rawUser
+    validationErrorComponent = either id (const nullError) <$> validationResult
     -- Error event is the sum of the event from the form and that of the server
-    errorEvent = leftmost [ updated $ either id (const nullError) <$> validationResult
+    errorEvent = leftmost [ updated validationErrorComponent
+                          , tagPromptlyDyn validationErrorComponent postBuild
                           , formErrorFromServer ]
     -- Here we retrieve only the error from the server events
     formErrorFromServer = fst . fanEither . snd . fanEither $ serverResponse
-    -- This is the event himself, to be used for button disabling
-    responseEvent = () <$ serverResponse
   -- In the end, I return both the dynamic containing the event and the raw
   -- event signal from the server. Probably the type could be changed slightly here.
+  -- display validationResult
   return (validationResult, serverResponse)
 
 type Endpoint t m = Dynamic t (Either Text User)
@@ -189,25 +196,26 @@ type Formlet t m = Event t :.: Const (Maybe Text) -.-> m :.: (Dynamic t)
 -- for this, or do only a smart constructor. But, if we want to write the type
 -- for the shaped drawingForm, it has to be a newtype.
 
-newtype FormletSimple t m a = FormletSimple (Dynamic t (Maybe Text) -> m (Dynamic t a))
+newtype FormletSimple t m a = FormletSimple (Event t () -> Dynamic t (Maybe Text) -> m (Dynamic t a))
 
 -- This is a generic function, just a way of zipping and sequencing the two parts
 createInterface :: forall t m . (MonadWidget t m)
   => UserShaped (Event t :.: Const (Maybe Text))
+  -> Event t ()
   -> UserShaped (FormletSimple t m)
   -> m (Dynamic t User)
-createInterface shapedError shapedFormlet = unComp . fmap SOP.to . SOP.hsequence $ hzipWith subFun a b
+createInterface shapedError e shapedFormlet = unComp . fmap SOP.to . SOP.hsequence $ hzipWith (subFun e) a b
   where
     a :: SOP.POP (Event t :.: Const (Maybe Text)) (Code User)
     a = singleSOPtoPOP . fromSOPI $ SOP.from shapedError
     b :: SOP.SOP (FormletSimple t m) (Code User)
     b = fromSOPI $ SOP.from shapedFormlet
 
-subFun :: MonadWidget t m => (Event t :.: Const (Maybe Text)) a -> FormletSimple t m a -> (m :.: Dynamic t) a
-subFun (Comp a) (FormletSimple f) = Comp $ do
+subFun :: MonadWidget t m => Event t () -> (Event t :.: Const (Maybe Text)) a -> FormletSimple t m a -> (m :.: Dynamic t) a
+subFun e (Comp a) (FormletSimple f) = Comp $ do
   let eventWithoutConst = getConst <$> a
   dynamicError <- holdDyn Nothing eventWithoutConst
-  f dynamicError
+  f e dynamicError
 
 -- Either move this temporary in shaped with the intent of reporting that
 -- upstream, or define a synonym. Discuss this on the generics-sop tracker!
